383fbb6855795426003cc8c8579edee2680466da
smititelu <stefan.mititelu92@gmail.com>
2015-12-31 06:12:32 -0800

    Refactor defs.py

--- tests/run_test.py
+++ tests/run_test.py
@@ -5,44 +5,91 @@ import os
 import time
 import re
 
-
+#################################### tests #####################################
 test_list = ['refer-3pcc', 'reinvite-3pcc', 'reinvite-proactive-3pcc', 'reinvite-proactive-extension-3pcc', 'join-3pcc', 'reinvite-uac', 'reinvite-uas', 'options-uas']
 
-# check if given number of calls is given as argument
+################################### checks ####################################
 if len(sys.argv) > 5:
-    try:
-        test_name = str(sys.argv[1])
-        test_title = str(sys.argv[2])
-        msg_nr = int(sys.argv[3])
-        rate_nr = int(sys.argv[4])
-        ratep_nr = int(sys.argv[5])
-    except Exception:
-        print("Usage: sudo python run_test.py [test_name] [test_title] [msg_nr] [rate_nr] [ratep_nr]")
-        exit()
+	try:
+		test_name = str(sys.argv[1])
+		test_title = str(sys.argv[2])
+		msg_nr = int(sys.argv[3])
+		rate_nr = int(sys.argv[4])
+		ratep_nr = int(sys.argv[5])
+	except Exception:
+		print("Usage: sudo python run_test.py [test_name] [test_title] [msg_nr] [rate_nr] [ratep_nr]")
+		exit()
 
 if not any(test_name in item for item in test_list):
-    print "Test " + test_name + " not found!"
-    exit()
+	print "Test " + test_name + " not found!"
+	exit()
 
-# global vars
-sip_server0_user, sip_server0_id, sip_server0_if, rtp_server0_if, sip_server0_port, rtp_server0_port, sip_server0_ip, rtp_server0_ip, sip_server0_file = "", "", "", "", "", "", "", "", ""
-sip_server1_user, sip_server1_id, sip_server1_if, rtp_server1_if, sip_server1_port, rtp_server1_port, sip_server1_ip, rtp_server1_ip, sip_server1_file = "", "", "", "", "", "", "", "", ""
+################################### globals ####################################
+# if
+sip_server0_if = defs.SERVER_IF_0
+sip_server1_if = defs.SERVER_IF_1
 
-sip_client0_user, sip_client0_id, sip_client0_if, rtp_client0_if, sip_client0_port, rtp_client0_port, sip_client0_ip, rtp_client0_ip, sip_client0_file = "", "", "", "", "", "", "", "", ""
-sip_client1_user, sip_client1_id, sip_client1_if, rtp_client1_if, sip_client1_port, rtp_client1_port, sip_client1_ip, rtp_client1_ip, sip_client1_file = "", "", "", "", "", "", "", "", ""
+rtp_server0_if = defs.SERVER_IF_0
+rtp_server1_if = defs.SERVER_IF_1
 
-sip_twin_port = 0 
+sip_client0_if = defs.CLIENT_IF_0
+sip_client1_if = defs.CLIENT_IF_1
 
+rtp_client0_if = defs.CLIENT_IF_0
+rtp_client1_if = defs.CLIENT_IF_1
+
+# ip
+sip_server0_ip = defs.SERVER_IP_0
+sip_server1_ip = defs.SERVER_IP_1
+
+rtp_server0_ip = defs.SERVER_IP_0
+rtp_server1_ip = defs.SERVER_IP_1
+
+sip_client0_ip = defs.CLIENT_IP_0
+sip_client1_ip = defs.CLIENT_IP_1
+
+rtp_client0_ip = defs.CLIENT_IP_0
+rtp_client1_ip = defs.CLIENT_IP_1
+
+# port
+sip_server0_port = defs.SERVER_PORT_00
+rtp_server0_port = defs.SERVER_PORT_01
+
+sip_server1_port = defs.SERVER_PORT_10
+rtp_server1_port = defs.SERVER_PORT_11
+
+sip_client0_port = defs.CLIENT_PORT_00
+rtp_client0_port = defs.CLIENT_PORT_01
+
+sip_client1_port = defs.CLIENT_PORT_10
+rtp_client1_port = defs.CLIENT_PORT_11
+
+sip_twin_port = defs.TWIN_PORT
+
+# users
+sip_server0_user = defs.SERVER_USER_0
+sip_server1_user = defs.SERVER_USER_1
+
+sip_client0_user = defs.CLIENT_USER_0
+sip_client1_user = defs.CLIENT_USER_1
+
+# xml files
+sip_server0_file = test_name + "/" + defs.SERVER_BASE_DIR + defs.SERVER_FILE_0
+sip_server1_file = test_name + "/" + defs.SERVER_BASE_DIR + defs.SERVER_FILE_1
+
+sip_client0_file = test_name + "/" + defs.CLIENT_BASE_DIR + defs.CLIENT_FILE_0
+sip_client1_file = test_name + "/" + defs.CLIENT_BASE_DIR + defs.CLIENT_FILE_1
+
+# other files
 order= test_name + "/order"
 result = test_name + "/time.out"
 
-# cleanup
+################################## cleanups ###################################
 try:
-    os.remove(order)
-    os.remove(result)
+	os.remove(order)
+	os.remove(result)
 except OSError:
-    pass
-
+	pass
 
 '''
 print "SERVER0 USER: " + sip_server0_user + " " + sip_server0_file + " " + sip_server0_ip + ":" + sip_server0_port + " " + rtp_server0_ip + ":" + rtp_server0_port
@@ -52,194 +99,100 @@ print "CLIENT1 USER: " + sip_client1_user + " " + sip_client1_file + " " + sip_c
 print "TWIN port: " + sip_twin_port
 '''
 
+################################## test run ###################################
 if re.search('3pcc', test_name):
-    # users
-    sip_server0_user = defs.SIP_3PCC_SERVER0_USER
-    sip_server1_user = defs.SIP_3PCC_SERVER1_USER
+	# start tcpdump capture
+	helps.tcpdump_start(test_name + "/" + test_name + ".pcap")
 
-    sip_client0_user = defs.SIP_3PCC_CLIENT0_USER
-    sip_client1_user = defs.SIP_3PCC_CLIENT1_USER
+	# start uas 1 - 3pcc B side
+	p = helps.sipp_3pcc_uas_start(
+		sip_server1_file,
+		sip_client1_ip, sip_client1_port,
+		sip_server1_ip, sip_server1_port,
+		rtp_server1_ip, rtp_server1_port,
+		sip_server0_ip, sip_twin_port,
+		sip_client1_user,
+		str(msg_nr), str(rate_nr), str(ratep_nr))
+	p.wait()
 
-    # id
-    sip_server0_id = defs.SIP_3PCC_SERVER0_ID
-    sip_server1_id = defs.SIP_3PCC_SERVER1_ID
+	# start uas 0 - 3pcc A side
+	p = helps.sipp_3pcc_uas_start(
+		sip_server0_file,
+		sip_client0_ip, sip_client0_port,
+		sip_server0_ip, sip_server0_port,
+		rtp_server0_ip, rtp_server0_port,
+		sip_server1_ip, sip_twin_port,
+		sip_client0_user,
+		str(msg_nr), str(rate_nr), str(ratep_nr))
+	p.wait()
 
-    sip_client0_id = defs.SIP_3PCC_CLIENT0_ID
-    sip_client1_id = defs.SIP_3PCC_CLIENT1_ID
+	# start uac 1
+	p = helps.sipp_3pcc_uac_start(
+		sip_client1_file,
+		sip_client1_ip, sip_client1_port,
+		rtp_client1_ip, rtp_client1_port,
+		sip_server1_ip, sip_server1_port,
+		sip_server1_user,
+		str(msg_nr), str(rate_nr), str(ratep_nr))
+	p.wait()
 
-    # if
-    sip_server0_if = defs.SIP_3PCC_SERVER0_IF
-    sip_server1_if = defs.SIP_3PCC_SERVER1_IF
-
-    rtp_server0_if = defs.RTP_3PCC_SERVER0_IF
-    rtp_server1_if = defs.RTP_3PCC_SERVER1_IF
-
-    sip_client0_if = defs.SIP_3PCC_CLIENT0_IF
-    sip_client1_if = defs.SIP_3PCC_CLIENT1_IF
-
-    rtp_client0_if = defs.RTP_3PCC_CLIENT0_IF
-    rtp_client1_if = defs.RTP_3PCC_CLIENT1_IF
-
-    # port
-    sip_server0_port = defs.SIP_3PCC_SERVER0_PORT
-    sip_server1_port = defs.SIP_3PCC_SERVER1_PORT
-
-    rtp_server0_port = defs.RTP_3PCC_SERVER0_PORT
-    rtp_server1_port = defs.RTP_3PCC_SERVER1_PORT
-
-    sip_client0_port = defs.SIP_3PCC_CLIENT0_PORT
-    sip_client1_port = defs.SIP_3PCC_CLIENT1_PORT
-
-    rtp_client0_port = defs.RTP_3PCC_CLIENT0_PORT
-    rtp_client1_port = defs.RTP_3PCC_CLIENT1_PORT
-
-    sip_twin_port = defs.SIP_3PCC_TWIN_PORT
-
-    # ip
-    sip_server0_ip = helps.get_ip_addr(sip_server0_if)
-    sip_server1_ip = helps.get_ip_addr(sip_server1_if)
-
-    rtp_server0_ip = helps.get_ip_addr(rtp_server0_if)
-    rtp_server1_ip = helps.get_ip_addr(rtp_server1_if)
-
-    sip_client0_ip = helps.get_ip_addr(sip_client0_if)
-    sip_client1_ip = helps.get_ip_addr(sip_client1_if)
-
-    rtp_client0_ip = helps.get_ip_addr(rtp_client0_if)
-    rtp_client1_ip = helps.get_ip_addr(rtp_client1_if)
-
-    # files
-    sip_server0_file = test_name + "/" + defs.SIPP_3PCC_BASE_DIR + defs.SIPP_3PCC_SERVER0_FILE
-    sip_server1_file = test_name + "/" + defs.SIPP_3PCC_BASE_DIR + defs.SIPP_3PCC_SERVER1_FILE
-
-    sip_client0_file = test_name + "/" + defs.SIPP_3PCC_BASE_DIR + defs.SIPP_3PCC_CLIENT0_FILE
-    sip_client1_file = test_name + "/" + defs.SIPP_3PCC_BASE_DIR + defs.SIPP_3PCC_CLIENT1_FILE
-
-    # start tcpdump capture
-    helps.tcpdump_start(test_name + "/" + test_name + ".pcap")
-
-    # start uas 1 - 3pcc B side
-    p = helps.sipp_3pcc_uas_start(
-        sip_server1_file,
-        sip_client1_ip, sip_client1_port,
-        sip_server1_ip, sip_server1_port,
-        rtp_server1_ip, rtp_server1_port,
-        sip_server0_ip, sip_twin_port,
-        sip_client1_user,
-        str(msg_nr), str(rate_nr), str(ratep_nr))
-    p.wait()
-
-    # start uas 0 - 3pcc A side
-    p = helps.sipp_3pcc_uas_start(
-        sip_server0_file,
-        sip_client0_ip, sip_client0_port,
-        sip_server0_ip, sip_server0_port,
-        rtp_server0_ip, rtp_server0_port,
-        sip_server1_ip, sip_twin_port,
-        sip_client0_user,
-        str(msg_nr), str(rate_nr), str(ratep_nr))
-    p.wait()
-
-    # start uac 1
-    p = helps.sipp_3pcc_uac_start(
-        sip_client1_file,
-        sip_client1_ip, sip_client1_port,
-        rtp_client1_ip, rtp_client1_port,
-        sip_server1_ip, sip_server1_port,
-        sip_server1_user,
-        str(msg_nr), str(rate_nr), str(ratep_nr))
-    p.wait()
-
-    # start uac 0
-    p = helps.sipp_3pcc_uac_start(
-        sip_client0_file,
-        sip_client0_ip, sip_client0_port,
-        rtp_client0_ip, rtp_client0_port,
-        sip_server0_ip, sip_server0_port,
-        sip_server0_user,
-        str(msg_nr), str(rate_nr), str(ratep_nr))
-    p.wait()
+	# start uac 0
+	p = helps.sipp_3pcc_uac_start(
+		sip_client0_file,
+		sip_client0_ip, sip_client0_port,
+		rtp_client0_ip, rtp_client0_port,
+		sip_server0_ip, sip_server0_port,
+		sip_server0_user,
+		str(msg_nr), str(rate_nr), str(ratep_nr))
+	p.wait()
 
 
 else:
-    # users
-    sip_server0_user = defs.SIP_3PCC_SERVER0_USER
-    sip_client0_user = defs.SIP_3PCC_CLIENT0_USER
+	# start tcpdump capture
+	helps.tcpdump_start(test_name + "/" + test_name + ".pcap")
 
-    # id
-    sip_server0_id = defs.SIP_3PCC_SERVER0_ID
-    sip_client0_id = defs.SIP_3PCC_CLIENT0_ID
+	# start uas 0
+	p = helps.sipp_uas_start(
+		sip_server0_file,
+		sip_client0_ip, sip_client0_port,
+		sip_server0_ip, sip_server0_port,
+		rtp_server0_ip, rtp_server0_port,
+		sip_client0_user,
+		str(msg_nr), str(rate_nr), str(ratep_nr))
+	p.wait()
 
-    # if
-    sip_server0_if = defs.SIP_3PCC_SERVER0_IF
-    rtp_server0_if = defs.RTP_3PCC_SERVER0_IF
-
-    sip_client0_if = defs.SIP_3PCC_CLIENT0_IF
-    rtp_client0_if = defs.RTP_3PCC_CLIENT0_IF
-
-    # port
-    sip_server0_port = defs.SIP_3PCC_SERVER0_PORT
-    rtp_server0_port = defs.RTP_3PCC_SERVER0_PORT
-
-    sip_client0_port = defs.SIP_3PCC_CLIENT0_PORT
-    rtp_client0_port = defs.RTP_3PCC_CLIENT0_PORT
-
-    # ip
-    sip_server0_ip = helps.get_ip_addr(sip_server0_if)
-    rtp_server0_ip = helps.get_ip_addr(rtp_server0_if)
-
-    sip_client0_ip = helps.get_ip_addr(sip_client0_if)
-    rtp_client0_ip = helps.get_ip_addr(rtp_client0_if)
-
-    #file
-    sip_server0_file = test_name + "/" + defs.SIPP_BASE_DIR + defs.SIPP_SERVER0_FILE
-    sip_client0_file = test_name + "/" + defs.SIPP_BASE_DIR + defs.SIPP_CLIENT0_FILE
-
-    # start tcpdump capture
-    helps.tcpdump_start(test_name + "/" + test_name + ".pcap")
-
-    # start uas 0
-    p = helps.sipp_uas_start(
-        sip_server0_file,
-        sip_client0_ip, sip_client0_port,
-        sip_server0_ip, sip_server0_port,
-        rtp_server0_ip, rtp_server0_port,
-        sip_client0_user,
-        str(msg_nr), str(rate_nr), str(ratep_nr))
-    p.wait()
-
-    # start uac 0
-    p = helps.sipp_uac_start(
-        sip_client0_file,
-        sip_client0_ip, sip_client0_port,
-        rtp_client0_ip, rtp_client0_port,
-        sip_server0_ip, sip_server0_port,
-        sip_server0_user,
-        str(msg_nr), str(rate_nr), str(ratep_nr))
-    p.wait()
+	# start uac 0
+	p = helps.sipp_uac_start(
+		sip_client0_file,
+		sip_client0_ip, sip_client0_port,
+		rtp_client0_ip, rtp_client0_port,
+		sip_server0_ip, sip_server0_port,
+		sip_server0_user,
+		str(msg_nr), str(rate_nr), str(ratep_nr))
+	p.wait()
 
 
 # wait for sipp scenario to stop
 while helps.process_is_running("sipp"):
-    1;
+	1;
 
 # stop tcpdump
 helps.tcpdump_stop()
 
 # start processing callflow flow only for one sip msg
 if msg_nr == 1:
-    # create order file used for ca
-    fd=open(order, "w+")
-    print >> fd, sip_client0_ip + ":.* UAC wlan0"
-    print >> fd, sip_server0_ip + ":.* UAS server0"
-    if sip_server0_ip != sip_server1_ip and sip_server1_ip != "":
-        print >> fd, sip_server1_ip + ":.* UAS server1"
-    if sip_client1_ip != "":
-        print >> fd, sip_client1_ip + ":.* UAC wlan1"
-    fd.close()
+	# create order file used for ca
+	fd=open(order, "w+")
+	print >> fd, sip_client0_ip + ":.* UAC wlan0"
+	print >> fd, sip_server0_ip + ":.* UAS server0"
+	if sip_server0_ip != sip_server1_ip and sip_server1_ip != "":
+		print >> fd, sip_server1_ip + ":.* UAS server1"
+	if sip_client1_ip != "":
+		print >> fd, sip_client1_ip + ":.* UAC wlan1"
+	fd.close()
 
-    p = helps.flow_start(test_name, test_title)
-    p.wait()
+	p = helps.flow_start(test_name, test_title)
+	p.wait()
 
 # start processing results
 p = helps.result_start(test_name, test_title)
