d68ac061833a17d5616ba09aae1567337d9aac49
smititelu <stefan.mititelu92@gmail.com>
2016-01-04 14:04:23 -0800

    Add myscript.py to process received location
    This python script computes euclidian distance between the first received
    geolocation position and the current geolocation position. Kamailio uses this
    python script to further process the location details received from the UAC.

---
+++ kamailio/myscript.py
@@ -0,0 +1,33 @@
+import Router
+import math
+from math import sqrt
+
+hysteresis = 4
+
+class ModuleClass:
+	def my_child_init(self, arg):
+		print 'Hello, child!'
+		return 1
+
+	def my_python_function(self, msg, args):
+#		Router.Logger.LM_INFO("args = " + args + "\n")
+		pos = [float(int(x)) for x in args.split()]
+
+		posX_first = pos[0]
+		posY_first = pos[1]
+		posX_last = pos[len(pos) - 2]
+		posY_last = pos[len(pos) - 1]
+#		Router.Logger.LM_INFO(str(posX_first) + " " + str(posY_first) + " " + str(posX_last) + " " + str(posY_last) + "\n")
+
+		euclid = math.sqrt((posX_last - posX_first) * (posX_last - posX_first) + (posY_last - posY_first) * (posY_last - posY_first))
+		Router.Logger.LM_INFO("euclidian distance=" + str(euclid) + "\n")
+
+		if (euclid > hysteresis):
+			Router.Logger.LM_INFO("euclidian distance=" + str(euclid) + " outside perimeter=" + str(hysteresis) + " HANDOVER !!!\n")
+		
+
+
+def my_mod_init():
+	print 'Hello, module!'
+	x = ModuleClass()
+	return x
